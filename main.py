217fd17500900ba100ea03a51040d1a8a4c35bd0
jorgeminguer16 <minguer94@gmail.com>
2023-01-11 14:34:49 -0500

    Se modifica archivo main

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-from flask import Flask
+from app import app
+from utils.db import db
 
-app = Flask(__name__)
-
-
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
-
+with app.app_context():
+    db.create_all()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(debug=True, host="0.0.0.0")
