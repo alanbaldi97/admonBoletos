c0cf9f956aef5cd409ed84b2176906e66846746b
jorgeminguer16 <minguer94@gmail.com>
2023-01-12 00:29:28 -0500

    Se modifica archivo principal main

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
-from app import app
 from utils.db import db
+from routes.eventos import evento
+from flask_sqlalchemy import SQLAlchemy
+from config import DATABASE_CONNECTION_URI
+
+app = Flask(__name__)
 
 with app.app_context():
     db.create_all()
+    from flask import Flask
+
+print(DATABASE_CONNECTION_URI)
+app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_CONNECTION_URI
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+# no cache
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+
+SQLAlchemy(app)
+
+app.register_blueprint(evento)
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
